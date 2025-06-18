116c09d28ebee920dd7f77cdbbd2794588ad309d
leball00niii <charrasenzo36@gmail.com>
2025-06-18 17:45:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,25 +15,30 @@
 
 <div id="board"></div>
 
+<!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Chessboard.js -->
 <script src="https://cdn.jsdelivr.net/npm/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js"></script>
 
 <script>
-  var board = Chessboard('board', {
-    pieceTheme: function(piece) {
-      var pieceName = '';
-      switch(piece[1].toLowerCase()) {
-        case 'p': pieceName = 'pion.png'; break;
-        case 'r': pieceName = 'tour.png'; break;
-        case 'n': pieceName = 'cavalier.png'; break;
-        case 'b': pieceName = 'fou.png'; break;
-        case 'q': pieceName = 'dame.png'; break;
-        case 'k': pieceName = 'roi.png'; break;
-      }
-      var colorFolder = (piece[0] === 'w') ? 'w/' : 'b/';
-      return 'img/chesspieces/' + colorFolder + pieceName;
-    },
-    position: 'start'
+  $(function() { // wait DOM ready
+    var board = Chessboard('board', {
+      pieceTheme: function(piece) {
+        var pieceName = '';
+        switch(piece[1].toLowerCase()) {
+          case 'p': pieceName = 'pion.png'; break;
+          case 'r': pieceName = 'tour.png'; break;
+          case 'n': pieceName = 'cavalier.png'; break;
+          case 'b': pieceName = 'fou.png'; break;
+          case 'q': pieceName = 'dame.png'; break;
+          case 'k': pieceName = 'roi.png'; break;
+        }
+        var colorFolder = (piece[0] === 'w') ? 'w/' : 'b/';
+        return 'img/chesspieces/' + colorFolder + pieceName;
+      },
+      position: 'start'
+    });
   });
 </script>
 
